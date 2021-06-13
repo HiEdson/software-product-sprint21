5ffa307bf85389f0b955149e58f376afa6d3a3f9
edson <edsoncasimiro264@gmail.com>
2021-06-13 14:39:46 +0000

    fetch hard-coded json data from server and display it randomly in the client-side

--- portfolio/src/main/webapp/fetch.js
+++ portfolio/src/main/webapp/fetch.js
@@ -1,7 +1,12 @@
 async function quote() {
-    const getServerSms = await fetch("/quoteSms");
-    const textOnly = await getServerSms.text();
+    const getServerSms = await fetch('/hello');
+    const allThoughs = await getServerSms.json();
+    //const myObj = await JSON.parse(allThoughs);
 
     const domElement = document.getElementById("fetch");
-    domElement.innerHTML = textOnly;
+    var t =Math.floor((Math.random() * 3));
+    domElement.innerHTML = allThoughs[Object.keys(allThoughs)[t]];
+
+    console.log(allThoughs[Object.keys(allThoughs)[t]]);
+    //console.log(myObj.though1); allThoughs.though1;
 }
